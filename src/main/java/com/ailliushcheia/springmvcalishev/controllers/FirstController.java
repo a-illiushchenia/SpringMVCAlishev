177478ad162c21970ea276cb1bce3a97234df8b2
Aleksandr Illiushchenia <a.illiushchenia@gmail.com>
2021-10-21 12:55:03 +0300

    Lesson 19. GET request parameters. Annotation @RequestParam

--- src/main/java/com/ailliushcheia/springmvcalishev/controllers/FirstController.java
+++ src/main/java/com/ailliushcheia/springmvcalishev/controllers/FirstController.java
@@ -3,13 +3,31 @@ package com.ailliushcheia.springmvcalishev.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + " " + surname);
+
+        return "first/hello";
+    }
+
+    @GetMapping("/hello1")
+    public String helloPage1(
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam("surname") String surname){
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
